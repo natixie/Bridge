a1f4710385b7ee6863c3005e489c5c4802087b32
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-02 00:00:50 +0200

    Testing

---
+++ BridgeTests/StoreBaeltCarTests.cs
@@ -0,0 +1,15 @@
+using Bridge;
+//using StoreBaeltTicketLibrary;
+
+namespace BridgeTests;
+
+//[TestClass]
+//public class StoreBaeltCarTests
+//{
+//    [TestMethod]
+//    public void StoreBaeltCar_Price_WeekdayWithoutBrobizz_ReturnsFullPrice()
+//    {
+//        var car = new StoreBaeltCarTests();
+//        car.Date = new DateTime(2025, 1, 10);
+//    }
+//}
--- StoreBaeltTicketLibrary/StoreBaeltMC.cs
+++ StoreBaeltTicketLibrary/StoreBaeltMC.cs
@@ -19,8 +19,6 @@ namespace StoreBaeltTicketLibrary
         {
             return "StoreBaelt MC";
         }
-        {
- 
-        }
+
     }
 }
