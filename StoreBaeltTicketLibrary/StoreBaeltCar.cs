b761816e8bbe634f82a1f9033a0a5f8abce3e643
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-01 23:01:45 +0200

    tilføjet StoreBaelt library med Car og MC classes

---
+++ StoreBaeltTicketLibrary/StoreBaeltCar.cs
@@ -0,0 +1,31 @@
+﻿using Bridge;
+using System;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltCar : Car
+    {
+        public override double Price(bool brobizz)
+        {
+            double price = base.Price();
+
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                price *= 0.85; //For at få 15% rabat
+            }
+
+            if (brobizz)
+            {
+                price *= 0.9; //For at få 10% rabat med brobizz
+            }
+
+            return price;
+        }
+
+        public override string VehicleType()
+        {
+            return "StoreBaelt Car";
+        }
+    }
+
+}
---
+++ StoreBaeltTicketLibrary/StoreBaeltMC.cs
@@ -0,0 +1,26 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    /// <summary>
+    /// Dette er en bil som crosser over StoreBaelts broen
+    /// </summary>
+    public class StoreBaeltMC : MC
+    {
+        /// <summary>
+        /// Returnere hvilken vehicle type det er
+        /// </summary>
+        public override string VehicleType()
+        {
+            return "StoreBaelt MC";
+        }
+        {
+ 
+        }
+    }
+}
