28d26955d3e87dec45f79fa14986b650c0d83771
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-01 05:47:19 +0200

    Har lavet Car class (ikke med tests endnu). Indsat License plate og datetime date properties, samt Price og Vehicle Type metode

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿namespace Bridge
+{
+    public class Car
+    {
+        public string Licenseplate {  get; set; }
+        public DateTime Date { get; set; }
+
+        
+        public double Price()
+        {
+            //Returnere en fixed pris
+            return 230;
+        }
+        
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+    }
+}
