c92e17ddf7f799132c9e384d819a6fde335701f9
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-01 07:05:37 +0200

    Har nu lavet BridgeTests og kørt dem successfuldt. Der var problematikker med ikke at kunne finde dll filen og ikke at kunne build solution, men har nu sat BridgeTests som startup project og fik endelig generet en Bridge.dll fil.

--- Car.cs
+++ Car.cs
@@ -1,11 +1,18 @@
-﻿namespace Bridge
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Bridge
 {
     public class Car
     {
         public string Licenseplate {  get; set; }
         public DateTime Date { get; set; }
 
-        
+        //Laver en constructor for at teste det i CarTests da jeg lavede en fejl tidligere
+        public Car(string licenseplate, DateTime date)
+        {
+            Licenseplate = licenseplate;
+            Date = date;
+        }
         public double Price()
         {
             //Returnere en fixed pris
