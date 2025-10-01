6aa1b2afbd3ad3ac485198f28b06f21f590e6234
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-02 01:56:30 +0200

    Tilføjet Øresundsbroens Car tests

---
+++ OresundBronTests/OresundCarTests.cs
@@ -0,0 +1,33 @@
+﻿using OresundbronTicketLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OresundBronTests
+{
+    [TestClass]
+    public class OresundCarTests
+    {
+        [TestMethod]
+        public void OresundCar_Price_WithoutBrobizzReturns460()
+        {
+            var car = new OresundCar();
+
+            // False fordi der testes for prisen uden brobizz rabat
+            var price = car.Price(false);
+
+            Assert.AreEqual(460.0, price, 0.01);
+        }
+
+        //Test for at prisen med brobizz rabat er 178 kr
+        [TestMethod]
+        public void OresundCar_Price_WithBrobizzReturns178()
+        {
+            var car = new OresundCar();
+
+            var price = car.Price(true);
+
+            Assert.AreEqual(178.0, price, 0.01);
+        }
+
+    }
+}
