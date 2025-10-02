a9753d1bdbbd53518f7f721d962e8c2afe187718
pandanati <72080264+pandanati@users.noreply.github.com>
2025-10-02 04:03:26 +0200

    Tilføjet IStoreBaeltRepository og interface

---
+++ StoreBaeltTicketLibrary/IStorebaeltRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    public interface IStoreBaeltRepository
+    {
+        void AddTicket(Vehicle vehicle);
+        List<Vehicle> GetAllTickets();
+        List<Vehicle> GetTicketsByLicensePlate(string licensePlate);
+        int GetTicketCount();
+        void ClearAllTickets();
+    }
+}
---
+++ StoreBaeltTicketLibrary/StorebaeltRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltRepository : IStoreBaeltRepository
+    {
+        private static readonly List<Vehicle> _tickets = new();
+
+        public void AddTicket(Vehicle vehicle) => _tickets.Add(vehicle);
+
+        public List<Vehicle> GetAllTickets() => _tickets;
+
+        public List<Vehicle> GetTicketsByLicensePlate(string licensePlate) =>
+            _tickets.Where(v => v.LicensePlate == licensePlate).ToList();
+
+        public int GetTicketCount() => _tickets.Count;
+
+        public void ClearAllTickets() => _tickets.Clear();
+    }
+}
